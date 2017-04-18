4d49153ae4807d6beb3beaa9d3c3399b47d6745f
Ramjee Ganti <ramjee.ganti@bigdecisions.com>
2017-04-18 14:33:41 +0530

    ran phpcbf inside data folder, a few files excluded

--- integration/whichbrowser/data/indices/models-s30plus.php
+++ integration/whichbrowser/data/indices/models-s30plus.php
@@ -2,19 +2,16 @@
 
 namespace WhichBrowser\Data;
 
-DeviceModels::$S30PLUS_INDEX = array (
-  '@21' => 
-  array (
-    0 => 215,
-  ),
-  '@22' => 
-  array (
-    0 => 220,
-    1 => 222,
-    2 => 225,
-  ),
-  '@23' => 
-  array (
-    0 => 230,
-  ),
+DeviceModels::$S30PLUS_INDEX = array(
+	'@21' => array(
+	0 => 215,
+	),
+	'@22' => array(
+	0 => 220,
+	1 => 222,
+	2 => 225,
+	),
+	'@23' => array(
+	0 => 230,
+	),
 );
